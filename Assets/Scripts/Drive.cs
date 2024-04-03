113176b3c56d69247d2729c79be3aefae9da5dc0
yurii <uraanenko9@gmail.com>
2024-04-03 23:00:48 +0300

    start

---
+++ Assets/Scripts/Drive.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Drive : MonoBehaviour {
+    public float speed = 10.0f;
+    public float rotationSpeed = 100.0f;
+    public Transform transGun;
+    public Transform gun;
+    public GameObject bulletObj;
+
+    // Update is called once per frame
+    void Update() {
+        float translation = Input.GetAxis("Vertical") * speed;
+        float rotation = Input.GetAxis("Horizontal") * rotationSpeed;
+
+        translation *= Time.deltaTime;
+        rotation *= Time.deltaTime;
+
+        transform.Translate(0, 0, translation);
+        // transform.Translate(0, 0, speed * Time.deltaTime);
+        transform.Rotate(0, rotation, 0);
+        if (Input.GetKey(KeyCode.T)) {
+            transGun.RotateAround(transGun.position, transGun.right, -2.0f);
+        } else if (Input.GetKey(KeyCode.G)) {
+            transGun.RotateAround(transGun.position, transGun.right, 2.0f);
+        } else if (Input.GetKeyDown(KeyCode.B)) {
+            Instantiate(bulletObj, gun.position, gun.rotation);
+        }
+    }
+}
